b92723c74a25a542514be295ce76133df573f5c5
NUC <NUC@INF-205-306-N01.ad.fh-albsig.de>
2019-01-07 17:39:56 +0100

    Mqtt message kann verschickt werden

--- client/src/main/java/de/fh/albsig/digitalfactory/client/MqttClientMain.java
+++ client/src/main/java/de/fh/albsig/digitalfactory/client/MqttClientMain.java
@@ -1,15 +1,24 @@
 package de.fh.albsig.digitalfactory.client;
 
+import java.util.Scanner;
+
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.fh.albsig.digitalfactory.connector.TaskSchedule;
+import de.fh.albsig.digitalfactory.connector.model.Task;
+
 public class MqttClientMain {
 
 	public static void main(final String[] args) throws MqttException {
 		final IMqttClient mqttClient = new MqttClient(
-				String.format("tcp://%s:%s", MqttConstants.BROKER, MqttConstants.BROKER_PORT), MqttClient.generateClientId());
+			String.format("tcp://%s:%s", MqttConstants.BROKER, MqttConstants.BROKER_PORT),
+			MqttClient.generateClientId()
+		);
 
 		final MqttConnectOptions options = new MqttConnectOptions();
 		options.setAutomaticReconnect(true);
@@ -19,8 +28,25 @@ public class MqttClientMain {
 		mqttClient.connect(options);
 
 		mqttClient.subscribe(MqttConstants.START_PROCESS, (topic, message) -> {
-			System.out.println(new String(message.getPayload()));
+			final String orderNumber = new String(message.getPayload());
+			System.out.println(orderNumber);
+
+			final TaskSchedule schedule = new TaskSchedule();
+			final Task task = schedule.nextTask(orderNumber);
+			System.out.println(task);
+
+			final String json = new ObjectMapper().writeValueAsString(task);
+			System.out.println(json);
+
+			mqttClient.publish(MqttConstants.TO_SPS, json.getBytes(), 1, true);
 		});
+
+
+		System.out.println("Press enter to exit!");
+		final Scanner s = new Scanner(System.in);
+		s.nextLine();
+
+		System.exit(0);
 	}
 
 }
